7e673aeb7aac6d923ed4bc7bef2f1ab068d765cc
sofiaso <91011159+sofiaso@users.noreply.github.com>
2022-04-14 18:51:31 +0300

    Add work

---
+++ Projects/TEAM-2/Stetsyuk_S/202/202/Program.cs
@@ -0,0 +1,10 @@
+﻿// 2. Напишіть програму, яка переводить гривні в $, Є.
+
+var sum = double.Parse(Console.ReadLine());
+Console.WriteLine("Рryvnias: " + Math.Round(sum, 2));
+
+var euro = sum / 32.08;
+Console.WriteLine("Euros: " + Math.Round(euro, 2));
+
+var dollar = sum / 29.3;
+Console.WriteLine("Dollars: " + Math.Round(dollar, 2));
