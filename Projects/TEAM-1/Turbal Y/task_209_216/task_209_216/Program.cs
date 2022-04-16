a39686afee233d75714b2c9b6933235bbf1a8fab
Yurii Turbal <103252117+TurbalY@users.noreply.github.com>
2022-04-16 11:52:09 +0300

    task_209

---
+++ Projects/TEAM-1/Turbal Y/task_209_216/task_209_216/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_209_216
+{
+    static class tasks_functions
+    {
+        /*Розробити програму, яка виводить на екран горизонтальну лінію з символів. 
+     Кількість символів, який використовувати символ, і яка буде лінія - вертикальна, або горизонтальна - 
+     вказує користувач.*/
+        public static int Row(char c, int number, byte flag)
+        {
+            int i;
+            if (flag == 0)
+                for (i = 0; i < number; i++) Console.Write(c);
+            else
+                for (i = 0; i < number; i++) Console.WriteLine(c);
+            return 0;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char c;
+            int n;
+            byte flag;
+            Console.WriteLine("Input symbol,number,flag(0/!0)");
+            c = char.Parse(Console.ReadLine());
+            n = int.Parse(Console.ReadLine());
+            flag = byte.Parse(Console.ReadLine());
+            tasks_functions.Row(c, n, flag);
+            Console.ReadLine();
+        }
+    }
+}
