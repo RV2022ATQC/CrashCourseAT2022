b0b719e33b7c76feb324b21343ad84af0e5888e3
Yurii Turbal <103252117+TurbalY@users.noreply.github.com>
2022-04-16 12:58:03 +0300

    +task210

--- Projects/TEAM-1/Turbal Y/task_209_216/task_209_216/Program.cs
+++ Projects/TEAM-1/Turbal Y/task_209_216/task_209_216/Program.cs
@@ -20,11 +20,30 @@ namespace task_209_216
                 for (i = 0; i < number; i++) Console.WriteLine(c);
             return 0;
         }
+        /*
+          Користувач вводить з клавіатури число - програма повинна показати скільки в даному числі цифр.
+          Число вводиться повністю в одну змінну.
+Примітка: Наприклад, користувач ввів число 11123445555.
+На екрані має з'явитися повідомлення про те, що в числі 5 цифр.
+         */
+        public static int DigitNumber(string s)
+        {
+            int count = 1;
+            for (int i = 1; i < s.Length; i++)
+            {
+                count++;
+                for (int j = 0; j < i; j++)
+                    if (s[j] == s[i]) { count--; break; }
+            }
+             return count;
+        }
+
     }
     class Program
     {
         static void Main(string[] args)
         {
+            /*
             char c;
             int n;
             byte flag;
@@ -32,7 +51,10 @@ namespace task_209_216
             c = char.Parse(Console.ReadLine());
             n = int.Parse(Console.ReadLine());
             flag = byte.Parse(Console.ReadLine());
-            tasks_functions.Row(c, n, flag);
+            tasks_functions.Row(c, n, flag);*/
+            Console.WriteLine("Enter the number");
+           string s = Console.ReadLine();
+           Console.WriteLine( tasks_functions.DigitNumber(s));
             Console.ReadLine();
         }
     }
