f0b1b006bcf20e0376feb633684dc82d28626ae7
MykolaKukla <135nickilay@gmail.com>
2022-04-17 11:00:17 +0300

    added task 201

---
+++ Projects/TEAM-1/Mykola Kukla/205.cs
@@ -0,0 +1,50 @@
+﻿
+
+
+
+
+
+
+
+// 5. Введіть три числа і виведіть на екран значення суми і добуток цих чисел.
+
+namespace Tasks
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string w;
+
+            int  a, b, с;
+
+            Console.WriteLine("Please enter the first digit ");
+
+            w = Console.ReadLine();
+
+            a = Convert.ToInt32(w);
+
+            Console.WriteLine("Please enter the second digit ");
+
+            w = Console.ReadLine();
+
+            b = Convert.ToInt32(w);
+
+            Console.WriteLine("Please enter the third digit ");
+
+           w = Console.ReadLine();
+
+            с = Convert.ToInt32(w);
+
+            int result = (a * b * с);
+
+            Console.WriteLine("The product of three numbers = " + result);
+
+
+
+
+        }
+
+    }
+}
