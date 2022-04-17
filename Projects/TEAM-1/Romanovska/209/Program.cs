54818c1b6c375e91f4d349d5aa82d58af9d506d2
A125-coder <57454044+A125-coder@users.noreply.github.com>
2022-04-17 21:34:00 +0300

    Edit task + add Tasks 207-219

---
+++ Projects/TEAM-1/Romanovska/209/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _209
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Розробити програму, яка виводить на екран горизонтальну лінію з символів. 
+             Кількість символів, який використовувати символ, і яка буде лінія - вертикальна, або 
+             горизонтальна - вказує користувач.*/
+
+            Console.WriteLine("Enter quantity and type of symbol you want to see, also what kind of line:" +
+                "vertical or horisontal:");
+            Console.WriteLine("Enter number of symbols: ");
+            int num_of_symbols = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter type of symbols: ");
+            string type_of_symbol = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter type of line: ");
+            string type_of_line = Convert.ToString(Console.ReadLine());
+            int i;
+
+            for(i=0; i<=num_of_symbols; i++)
+            {
+                if(type_of_line == "horizontal")
+                {
+                    Console.Write($" {type_of_symbol}");
+                }
+                else
+                {
+                    Console.WriteLine($"{type_of_symbol}");
+                }
+            }
+        }
+    }
+}
