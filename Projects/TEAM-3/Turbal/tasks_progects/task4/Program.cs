330af47cd847e2276dd1f8e30c7d851c1dd3e992
Maritcka <102948818+Maritcka@users.noreply.github.com>
2022-04-18 12:39:48 +0300

    edit files
    edit files

---
+++ Projects/TEAM-3/Turbal/tasks_progects/task4/Program.cs
@@ -0,0 +1,17 @@
+﻿//4. Напишіть програму, яка обчислює квадрат будь-якого, введеного числа.
+
+float n = 0, n_square = 0;
+
+Console.WriteLine("Please, input number:");
+try
+{
+    n = Convert.ToSingle(Console.ReadLine());
+    n_square = n * n;
+    Console.WriteLine($"n^2={n_square}");
+}
+catch
+{
+    Console.WriteLine("Input is not a number!");
+}
+
+
