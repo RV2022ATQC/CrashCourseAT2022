cc24b1c131e99f151658c698182013903513b621
sofiaso <91011159+sofiaso@users.noreply.github.com>
2022-04-18 16:48:47 +0300

    Add Stetsyuk_S 208-215

---
+++ Projects/TEAM-2/Stetsyuk_S/208-215/208/Program.cs
@@ -0,0 +1,53 @@
+﻿//3. Відомо, що 1 дюйм дорівнює 2.54 см.
+//Розробити додаток, що переводять дюйми в сантиметри і навпаки.
+//Діалог з користувачем реалізувати через систему меню
+
+Console.Write("Enter length: ");
+var length = double.Parse(Console.ReadLine());
+var value = 'c';
+while (true)
+{
+    Console.WriteLine("Enter 1 to convert to inches.");
+    Console.WriteLine("Enter 2 to convert to centimeters.");
+    Console.Write("Enter 3 to stop.\nChoise: ");
+    var choise = int.Parse(Console.ReadLine());
+    if (choise == 3)
+        return;
+
+    if (value == 'c')
+    {
+        switch (choise)
+        {
+            case 1:
+                length /= 2.54;
+                value = 'i';
+                Console.WriteLine(Math.Round(length, 3) + " inches");
+                break;
+            case 2:
+                length *= 1;
+                Console.WriteLine(Math.Round(length, 3) + " centimeters");
+                break;
+            default:
+                Console.WriteLine("Enter correct number");
+                break;
+        }
+    }
+    else
+    {
+        switch (choise)
+        {
+            case 2:
+                length *= 2.54;
+                value = 'c';
+                Console.WriteLine(Math.Round(length, 3) + " centimeters");
+                break;
+            case 1:
+                length *= 1;
+                Console.WriteLine(Math.Round(length, 3) + " inches");
+                break;
+            default:
+                Console.WriteLine("Enter correct number");
+                break;
+        }
+    }
+}
---
+++ Projects/TEAM-2/Stetsyuk_S/208-215/215/Program.cs
@@ -0,0 +1,25 @@
+﻿//7.Написати програму, яка виводить на екран - наступну фігуру:
+//*********************                                                                                                                                                                                                        
+//*                   *
+//*                   *
+//*                   *
+//*                   *
+//*********************
+//ширина і висота фігури встановлюються користувачем з клавіатури.
+
+Console.Write("Enter height: ");
+var height = int.Parse(Console.ReadLine());
+Console.Write("Enter width: ");
+var width = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < height; i++)
+{
+    for (int j = 0; j < width; j++)
+    {
+        if ((j == 0) || (j == width - 1) || (i == 0) || (i == height - 1))
+            Console.Write("*");
+        else
+            Console.Write(" ");
+    }
+    Console.WriteLine();
+}
