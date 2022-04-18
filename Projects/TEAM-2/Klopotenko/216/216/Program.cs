badd431fe69e170a76e6b3c8c5a5017f9f20c924
Vlad <vladklopotenko@gmail.com>
2022-04-18 22:22:31 +0200

    Add Task 216

---
+++ Projects/TEAM-2/Klopotenko/216/216/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 216. Написати програму, яка виводить вміст масиву навпаки.
+Приклад: масив 23 11 6 перетворюється в 6 11 23. */
+
+
+
+
+using System;
+
+namespace _216
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[3] { 23, 11, 6 };
+            int length = array.Length;
+            Console.Write("The contents of the array on the contrary: ");
+            for (int i = --length; i >= 0; i--)
+            {
+                Console.Write(array[i] + " ");
+            }
+            
+        }
+    }
+}
