ddfb8ec87890ee061ebcb435b1ee9355ab0b662d
MykolaKukla <135nickilay@gmail.com>
2022-04-20 21:57:58 +0300

    added task 206

---
+++ Projects/TEAM-1/Mykola Kukla/206/206/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+// 5. Введіть три числа і виведіть на екран значення суми і добуток цих чисел.
+
+namespace Tasks
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a;
+
+            a = int.Parse(Console.ReadLine());
+
+            int temp = a % 2;
+
+            bool result = temp == 0;
+
+            if (result) 
+            {
+              Console.WriteLine("Even number");
+            }
+            else
+            {
+              Console.WriteLine("Odd number");
+            }
+
+
+
+
+
+        }
+    }
+}
