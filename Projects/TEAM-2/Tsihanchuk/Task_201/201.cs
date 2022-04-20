bc2f65ec64cdeec171f05dfcbab4e6129f4f64b3
Максим Циганчук <102756905+ShiroMx@users.noreply.github.com>
2022-04-20 18:34:36 +0300

    Add my file
    And add first task

---
+++ Projects/TEAM-2/Tsihanchuk/Task_201/201.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace AVG;
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = Console.ReadLine(); \\first number
+            int second = Console.ReadLine();\\second number
+
+            float avg=(first+second)\2
+
+            System.WriteLine(avg);
+        }
+    }
+}
