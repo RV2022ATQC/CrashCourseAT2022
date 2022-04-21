762e786890fcea6bcac571e151373d48b8b2c95d
Maksym <makcim2004leb@gmail.com>
2022-04-21 19:42:02 +0300

    Update Program.cs

--- Projects/TEAM-1/Lebid Maksym/Task220/Task220/Program.cs
+++ Projects/TEAM-1/Lebid Maksym/Task220/Task220/Program.cs
@@ -14,11 +14,10 @@ namespace Task220
             //Написати overload методи Add які в залежності від параметрів або додають цифрові значення, або об'єднують введені тексти.
             //Передбачити аргументи за замовчуванням і використання статичних полів\методів
 
-            Console.WriteLine("1 for numbers, 0 for text");
-            int t1; //= Convert.ToInt32(Console.ReadLine());
+            
+            int t1; 
             int t2;
             
-           
             Console.WriteLine("Enter first object");
             var i1 = Convert.ToString(Console.ReadLine());
 
@@ -35,19 +34,6 @@ namespace Task220
             {
                 Add(i1, i2);
             }
-
-            /*Add(i1, i2);
-            
-            if (t1 == 0)
-            {
-                Console.WriteLine("Enter first object");
-                string s1 = Console.ReadLine();
-
-                Console.WriteLine("Enter second object");
-                string s2 = Console.ReadLine();
-
-                Add(s1, s2);
-            }*/
 
         }
         static void Add(int x, int y)
