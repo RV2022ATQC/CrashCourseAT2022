f530c420f60e7aba2c89886b305b69d2eb079d63
Serhii Tarasiuk <serhiy.slm@gmail.com>
2022-04-21 13:48:29 +0300

    add task 208

---
+++ Projects/TEAM-1/Tarasiuk S/Task_208/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task208
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//Create program to convert cm into inches OR inches into cm DEPENDING user choice
+            Console.WriteLine("Enter you number");
+            double number = 0;
+            number = Convert.ToSingle(Console.ReadLine());    // вводимо число
+
+            Console.WriteLine("Chose \n" +
+                "U want conver cm into inches INPUT 1 \n" +
+                "U want conver cm into inches INPUT 2");   // вибираємо що з ним робити        
+            var choise = 0;
+
+            choise = (int)Convert.ToSingle(Console.ReadLine());
+
+            double converted = 0;     
+
+            if (choise == 1)       
+            {
+                converted = number * 2.54;
+                Console.WriteLine($"{converted} inches");  
+            }
+            else if (choise == 2)
+            {
+                converted = number / 2.54;
+                Console.WriteLine($"{converted} cm");
+            }
+
+            else
+                Console.WriteLine("Error your choice is not valid. Please try again and read all condition :)");
+        }
+    }
+}
