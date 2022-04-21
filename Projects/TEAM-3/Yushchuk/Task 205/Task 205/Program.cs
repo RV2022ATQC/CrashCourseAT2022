683c312eeab03ad2b61f7dbb84e864f9f9d6951b
Fedorenkow <oleh.yushchuk@gmail.com>
2022-04-21 18:43:56 +0300

    add Task 201-207

---
+++ Projects/TEAM-3/Yushchuk/Task 205/Task 205/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_205{
+    class Program{
+        /* 
+         --------------------------------------------------------------------------------------
+        Task 205: 5. Введіть три числа і виведіть на екран значення суми і добуток цих чисел.
+         --------------------------------------------------------------------------------------
+         */
+        static void Main(string[] args){
+            double a, b, c,sum, mult;
+
+            Console.WriteLine("Enter 3 number: ");
+
+            Console.WriteLine("a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("c: ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            sum = a + b + c;
+            mult = a * b * c;
+
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Multipiclation: " + mult);
+
+
+        }
+    }
+}
---
+++ Projects/TEAM-3/Yushchuk/Task 206/Task 206/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task_206
+{
+    class Program{
+        /* 
+         ---------------------------------------------------------------------------------------
+         Task 206: 1. Напишіть програму, яка перевіряє число, введене з клавіатури на парність.
+         ---------------------------------------------------------------------------------------
+        */
+        static void Main(string[] args) {
+            double a;
+
+            Console.WriteLine("Enter number for check on even or odd: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            if (a % 2 == 0) {
+                Console.WriteLine("Number is even");
+            }
+            else{
+                Console.WriteLine("Number is odd");
+            }
+
+        }
+    }
+}
