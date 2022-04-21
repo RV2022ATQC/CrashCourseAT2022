1b923d57286ee40b0ede11972a01731dfad4ea79
Maksym <makcim2004leb@gmail.com>
2022-04-21 19:40:54 +0300

    Update Program.cs

--- Projects/TEAM-1/Lebid Maksym/Task220/Task220/Program.cs
+++ Projects/TEAM-1/Lebid Maksym/Task220/Task220/Program.cs
@@ -15,19 +15,30 @@ namespace Task220
             //Передбачити аргументи за замовчуванням і використання статичних полів\методів
 
             Console.WriteLine("1 for numbers, 0 for text");
-            int t = Convert.ToInt32(Console.ReadLine());
+            int t1; //= Convert.ToInt32(Console.ReadLine());
+            int t2;
+            
+           
+            Console.WriteLine("Enter first object");
+            var i1 = Convert.ToString(Console.ReadLine());
 
-            if (t == 1)
+            Console.WriteLine("Enter second object");
+            var i2 = Convert.ToString(Console.ReadLine());
+
+            try
             {
-                Console.WriteLine("Enter first object");
-                int i1 = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Enter second object");
-                int i2 = Convert.ToInt32(Console.ReadLine());
-
+                t1 = Convert.ToInt32(i1);
+                t2 = Convert.ToInt32(i2);
+                Add(t1, t2);
+            }
+            catch (Exception ex)
+            {
                 Add(i1, i2);
             }
-            else if (t == 0)
+
+            /*Add(i1, i2);
+            
+            if (t1 == 0)
             {
                 Console.WriteLine("Enter first object");
                 string s1 = Console.ReadLine();
@@ -36,7 +47,7 @@ namespace Task220
                 string s2 = Console.ReadLine();
 
                 Add(s1, s2);
-            }
+            }*/
 
         }
         static void Add(int x, int y)
