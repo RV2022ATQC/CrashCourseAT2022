ed3950f195bfb6280cfdd62e779db51f06457dca
sofiaso <91011159+sofiaso@users.noreply.github.com>
2022-04-28 14:48:00 +0300

    add 216-219

---
+++ Projects/TEAM-2/Stetsyuk_S/208-219/217/Program.cs
@@ -0,0 +1,40 @@
+﻿//3.Написати програму, яка знаходить в масиві значення, 
+//  що повторюються два і більше разів, і показує їх на екран.
+
+using System.Linq;
+
+List <int> l = new List<int> ();
+Random r = new Random ();
+
+for (int i = 0; i < 10; i++)
+{
+    l.Add(r.Next(0, 10));
+}
+
+Console.Write("Array: ");
+foreach (int el in l)
+{
+    Console.Write(el + " ");
+}
+
+Console.WriteLine();
+
+List<int> l1 = new List<int>();
+
+for (int i = 0;i < l.Count(); i++)
+{
+    for(int j = i + 1; j < l.Count(); j++)
+    {
+        if (l[i] == l[j])
+        {
+            l1.Add(l[i]);
+        }
+    }
+}
+
+var l2 = l1.Distinct();
+Console.Write("Digit which are dublicated: ");
+foreach (int el in l2)
+{
+    Console.Write(el + " ");
+}
---
+++ Projects/TEAM-2/Stetsyuk_S/208-219/219/Program.cs
@@ -0,0 +1,25 @@
+﻿//4.Написати програму, яка знаходить в масиві найменше непарне число і показує його на екран.
+using System.Linq;
+
+List<int> l = new List<int> ();
+Random random = new Random ();
+
+for (int i = 0; i < 10; i++)
+{
+    l.Add (random.Next(0, 10));
+}
+
+Console.Write("Array: ");
+foreach (int i in l)
+{
+    Console.Write(i + " ");
+}
+
+int min = l[0];
+foreach (int i in l)
+{
+    if ((i < min) && (i % 2 != 0))
+        min = i;
+}
+
+Console.WriteLine("\nMin and odd: " + min);
