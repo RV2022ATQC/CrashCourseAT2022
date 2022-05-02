77941e513a844c4b19fc788f8971f55456a6fb1a
LoveMyDaughter <tsav1986@gmail.com>
2022-05-02 17:10:06 +0300

    Tsvihun add Task 201 Java

--- Projects/TEAM-2/Tsvihun/JAVA/Task_201/src/Main.java
+++ Projects/TEAM-2/Tsvihun/JAVA/Task_201/src/Main.java
@@ -1,7 +1,21 @@
+/*
+201.
+Напишіть програму, яка обчислює середнє арифметичне двох чисел.
+*/
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// 201
-        System.out.println("Hi, Antonio !");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        var firstNumber = input.nextFloat();
+
+        System.out.println("Enter second number: ");
+        var secondNumber = input.nextFloat();
+
+        System.out.println((firstNumber + secondNumber) / 2);
     }
 }
