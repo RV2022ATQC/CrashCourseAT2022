ab6f055f698a209db8a2ea7e25e114176e205468
Yurii Turbal <103252117+TurbalY@users.noreply.github.com>
2022-05-03 23:23:54 +0300

    last variant

---
+++ Projects/TEAM-1/team1_group_proj/team1_group/team1_group/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace team1_group
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (military_unit.space[100, 200] == null) Console.WriteLine("spase is empty");
+                abrams a = new abrams(100,200,20,10,100,0,"Abrams1");
+            if (military_unit.space[100, 200] != null)
+            {
+                //з простором можна працювати як military_unit.space[x, y] або abrams.space[x,y]!
+                abrams b = (abrams)military_unit.space[100, 200];
+                Console.WriteLine("Hello! I am "+b.getname());
+            }            Console.ReadLine();
+        }
+    }
+}
---
+++ Projects/TEAM-1/team1_group_proj/team1_group/team1_group/my_unit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace team1_group
+{
+    class abrams : military_unit
+    {
+        public abrams() { }
+        public abrams(int _x, int _y, int _ammun, int _defence, int _fuel, byte _army_name, string _name)
+            : base(_x, _y, _ammun, _defence, _fuel, _army_name)
+        { name = _name;
+            space[_x, _y] = this;
+        }
+        //функція руху
+        public override void move()
+        {
+            x++;
+            y++;
+        }
+        //функція атаки обєкта
+        public override void attack(military_unit a)
+        {
+            a.defense -= 1;
+            if (a.defense == 0) space[a.x, a.y] = null;
+        }
+
+        public string getname()
+        {
+            return name;
+        }
+
+    }
+}
