994340ae7f94e042184964057bffcb22ad34d40d
ShiroMx <tsyhanchuk_ak21@nuwm.edu.ua>
2022-05-03 17:00:13 +0300

    Add to task_202

---
+++ Projects/TEAM-2/Tsihanchuk/Tasks/Task_202/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_202
+{
+    internal class Program
+    {   //2. Напишіть програму, яка переводить гривні в $, Є.
+        static void Main(string[] args)
+        {
+            float usd = 0;
+            Console.Write("UAH: ");
+            usd = Int32.Parse(Console.ReadLine());
+
+            Console.Write("In dolar " +Math.Round(usd / 29.6,2 ));
+            Console.WriteLine("\nIn evro " + Math.Round(usd / 32.1, 2));
+        }
+    }
+}
