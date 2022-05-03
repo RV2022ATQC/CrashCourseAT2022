5afb65a0080696c41a30a56e4f016dd20c262576
rewqla <desares526@gmail.com>
2022-05-03 18:18:54 +0300

    Add 220-226

---
+++ Projects/TEAM-2/Mosiichuck/224/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _224
+{
+    class Program
+    {
+        //Дано масив випадкових чисел в діапазоні від -20 до +20. Необхідно знайти позиції крайніх негативних елементів
+        //(самого лівого негативного елементу і самого правого негативного елементу) і впорядкувати елементи, що знаходяться між ними.
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] arr = new int[15];
+            int? positionf = null, positionl = null;
+            Console.Write("Your array: ");
+            for (int i = 0; i < 15; i++)
+            {
+                arr[i] = rnd.Next(-20, 20);
+                if (positionf == null && arr[i] < 0)
+                    positionf = i;
+                else if (arr[i] < 0)
+                    positionl = i;
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            Array.Sort(arr, (int)positionf, (int)positionl);
+            Console.Write("Your sorted array: ");
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Projects/TEAM-2/Mosiichuck/226/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _226
+{
+    class Program
+    {
+        //Написати функцію, яка сортує масив  розмірністю 10 елементів за зростанням або за спаданням, в залежності від третього параметра функції.
+        //Якщо він дорівнює 1, сортування йде за спаданням, якщо 0, то по зростанню. Перші 2 параметра функції - це масив і його розмір,
+        //третій параметр за замовчуванням дорівнює 1.
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] arr = new int[10];
+            Console.Write("Your array: ");
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = rnd.Next(-20, 20);
+                Console.Write(arr[i] + " ");
+            }
+            Console.Write("\nCount of elements: ");
+            int count = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("1 - sort by descending\n0 - sort by increasing");
+            int flag = Int32.Parse(Console.ReadLine());
+            SortArray(arr, count, flag);
+        }
+
+        static void SortArray(int[] arr,int lenght,int flag=1)
+        {
+            if (flag == 1)
+            {
+                Array.Sort(arr, 0, lenght);
+                Array.Reverse(arr, 0, lenght);
+
+            }
+            else if(flag ==0)
+            {
+                Array.Sort(arr, 0, lenght);
+            }
+            Console.Write("Sorted array: ");
+            foreach (var item in arr)
+            {
+                Console.Write(item+ " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
