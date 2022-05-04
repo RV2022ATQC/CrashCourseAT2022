b2b0236ef232fa7dc83f0a1eb23e2ce87547634a
Denys98 <rokhmanyk@gmail.com>
2022-05-04 10:33:55 +0300

    Task 201_rokhmaniuk done!

--- Projects/TEAM-2/Rokhmaniuk/Task_ 201/Program.cs
+++ Projects/TEAM-2/Rokhmaniuk/Task_ 201/Program.cs
@@ -1,6 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, US army!");
-Console.WriteLine("ми з україни!");
+﻿/*
+201.
+Напишіть програму, яка обчислює середнє арифметичне двох чисел.
+*/
+using System;
+
+public class Average
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter first number: ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter second number: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Average is: {(num1 + num2) / 2}");
+
+    }
+}
 
 
