a60d1d5dcf359a3cab0193bc6870c600faa4738a
Фугель Владислав <102908868+Fyhel-Vladyslav@users.noreply.github.com>
2022-05-05 01:09:18 +0300

    add my class Vilkha_unit
    Both i changed Program.cs, for spawning my units

--- Projects/TEAM-1/team1_group_proj/team1_group/team1_group/Program.cs
+++ Projects/TEAM-1/team1_group_proj/team1_group/team1_group/Program.cs
@@ -20,15 +20,24 @@ namespace team1_group
                 Console.WriteLine("Hello! I am "+b.getname());
             }*/
             //породжуэмо танчики випадково
-            Random rnd = new Random();
-            for (int i = 0; i < 10; i++)
+            Random rnd = new Random();      
+
+
+            for (int i = 0; i < 20; i++)
             {
                 int x= rnd.Next(500), y= rnd.Next(500);
-                military_unit.space[x, y] = new abrams(x, y, 20, 10, 100,rnd.Next(2).ToString(), "Abrams1");
-
+                military_unit.space[x, y] = new abrams(x, y, 20, 30, 100,rnd.Next(2).ToString(), "Abrams1");
+            }
+            for (int i = 0; i < 16; i++)
+            {
+                int x = rnd.Next(500), y = rnd.Next(500);
+                if (military_unit.space[x, y] == null)
+                    military_unit.space[x, y] = new Vilkha_unit(x, y, 50, 20, 50, rnd.Next(2).ToString(), "Vilkha");
+                else
+                    i--;
             }
             //загальний ігровий цикл
-        for (int i=0;i<15;i++)
+            for (int i=0;i<15;i++)
             {
                 //перегляд всіх об'єктів та їх дія
                 for (int j = 0; j < 500; j++)
