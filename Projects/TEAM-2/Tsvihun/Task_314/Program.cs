2874727fd26b9b192fe31a799ecfedb652d0883a
LoveMyDaughter <tsav1986@gmail.com>
2022-05-12 00:20:11 +0300

    Tsvihun Add task 314

---
+++ Projects/TEAM-2/Tsvihun/Task_314/Program.cs
@@ -0,0 +1,28 @@
+﻿// 314
+//Створіть клас Student, який буде містити інформацію про студента. За допомогою механізму наслідування,
+// реалізуйте клас Aspirant (аспірант - студент, який готується до захисту кандидатської роботи) похідний від Student.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_314
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student vovchik = new Student("Volodya Abramovych", 4);
+            vovchik.StudentInfo();
+
+            Student misha = new Student ("Misha Konchuk", 5);
+            misha.StudentInfo();
+
+            Aspirant galatyuk = new Aspirant("Mykhailo Galatyuk", 6, "Polymers processes");
+            galatyuk.StudentInfo();
+
+        }
+    }
+}
