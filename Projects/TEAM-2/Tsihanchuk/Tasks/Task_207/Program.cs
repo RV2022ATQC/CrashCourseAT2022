8074c1bc634f5fb7406e5426836cce9880a60794
ShiroMx <tsyhanchuk_ak21@nuwm.edu.ua>
2022-05-12 00:21:41 +0300

    Add task 207

---
+++ Projects/TEAM-2/Tsihanchuk/Tasks/Task_207/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _207
+{
+    //2. Дано натуральне число а (a <100). Напишіть програму, що виводить на екран кількість цифр в цьому числі і суму цих цифр
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            
+            System.Console.Write("Enter a number in array of (0<a<100) ");
+            number=Int32.Parse( System.Console.ReadLine());
+            {
+                if(number>=100||number<0)
+                {
+                    Console.WriteLine("Incorrect value");
+                }
+                else 
+                    {
+                    if(number>9)
+                    {
+                        Console.WriteLine("We have two number");
+                        Console.WriteLine("Sum of number is:" + ((number / 10) + (number % 10)));
+                    }else
+                        {
+                        Console.WriteLine("We have one number");
+                        Console.WriteLine("Sum of number is:" + number);
+                        }
+
+                    }
+            }
+            
+        }
+    }
+}
