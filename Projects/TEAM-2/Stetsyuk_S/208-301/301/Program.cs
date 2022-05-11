b92f3a220b007517350983ec9f3e88f7e9b4b992
sofiaso <91011159+sofiaso@users.noreply.github.com>
2022-05-11 14:44:25 +0300

    Add 301

---
+++ Projects/TEAM-2/Stetsyuk_S/208-301/301/Program.cs
@@ -0,0 +1,41 @@
+﻿using _301;
+
+//Написати клас Товар. Також реалізувати клас магазин. 
+//Передбачити можливість отримання товару на баланс і продажу товару.
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Item> items = new List<Item>();
+        items.Add(new Item("name1", 123.6, "Creator1", 5));
+        items.Add(new Item("name2", 190, "Creator2", 1));
+        Shop shop = new Shop(items);
+
+        foreach(Item item in shop.Items)
+        {
+            Console.WriteLine(item);
+        }
+
+        shop.Buy("name1");
+        shop.Buy("name2");
+
+        Console.WriteLine("---------------------------------------------------------------------------");
+
+        foreach (Item item in shop.Items)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine("---------------------------------------------------------------------------");
+
+        shop.Add(new Item("name2", 190, "Creator2", 1));
+        shop.Add(new Item("name1", 123.6, "Creator1", 5));
+        shop.Add(new Item("name1", 123.6, "Creator3", 5));
+        
+        foreach (Item item in shop.Items)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
