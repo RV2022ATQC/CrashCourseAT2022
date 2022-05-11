5076b8cf7f7aef783938f792e1bf75c92651434f
ShiroMx <tsyhanchuk_ak21@nuwm.edu.ua>
2022-05-12 00:45:19 +0300

    Add task 208

---
+++ Projects/TEAM-2/Tsihanchuk/Tasks/Task_208/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_208
+{//3. Відомо, що 1 дюйм дорівнює 2.54 см. Розробити додаток, що переводять дюйми в сантиметри і навпаки. Діалог з користувачем реалізувати через систему меню.
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const double inch = 2.54;
+            double result, value;
+            int mode;
+
+            Console.WriteLine("1.Inch to centimeters");
+            Console.WriteLine("2.Sentimeters to inch");
+
+            mode = Int32.Parse( Console.ReadLine() );
+
+            if (mode == 1)
+            {
+                Console.WriteLine("Enter inch:");
+                value=Int32.Parse( Console.ReadLine() );
+
+                result = value * 2.54;
+
+                Console.WriteLine(result);
+            }else
+                {
+                Console.WriteLine("Enter centimeters:");
+                value = Int32.Parse(Console.ReadLine());
+
+                result = value / inch;
+                Console.WriteLine(result);
+
+            }
+
+
+        }
+    }
+}
