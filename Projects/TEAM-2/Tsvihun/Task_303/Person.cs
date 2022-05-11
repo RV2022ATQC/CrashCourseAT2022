73a66fef1ace683863bba79fba950d75b4ad2573
LoveMyDaughter <tsav1986@gmail.com>
2022-05-11 16:27:17 +0300

    Tsvihun Add task 303

---
+++ Projects/TEAM-2/Tsvihun/Task_303/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_303
+{
+    internal class Person
+    {
+        string name;
+        int age;
+        string sex;
+        string phone;
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public string Sex { get => sex; set => sex = value; }
+        public string Phone { get => phone; set => phone = value; }
+
+        public Person() { }
+        public Person(string name, int age, string sex, string phone)
+        {
+            this.name = name;  
+            this.age = age;
+            this.sex = sex;
+            this.phone = phone;
+        }
+
+        public void Print ()
+        {
+            Console.WriteLine ($"Name: {Name}\nAge: {Age}\nSex: {Sex}\nPhone: {Phone}");
+        }
+    }
+}
