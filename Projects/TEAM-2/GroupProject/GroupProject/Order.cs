6d5262d03ef353d6ffe8e5d015aa2a16695514c4
mertsalovaa <irynamertsalova@gmail.com>
2022-05-12 18:04:11 +0300

    update Order.cs

--- Projects/TEAM-2/GroupProject/GroupProject/Order.cs
+++ Projects/TEAM-2/GroupProject/GroupProject/Order.cs
@@ -1,4 +1,4 @@
-﻿// Мерцалова Ірина
+﻿// Мерцалова Ірина, Мосійчук Юрій
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +7,28 @@ using System.Threading.Tasks;
 
 namespace GroupProject
 {
-    internal class Order
+    public class Order
     {
+        private int id;
+        private DateTime date;
+        private bool status;
+
+        public int Id 
+        {
+            get { return id; }
+            set { id = value; } 
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public bool Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
     }
 }
