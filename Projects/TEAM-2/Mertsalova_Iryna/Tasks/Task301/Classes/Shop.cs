8ebf6252c0dd821ec7025d43c22f5a318194ab1a
mertsalovaa <irynamertsalova@gmail.com>
2022-05-12 20:53:34 +0300

    task 301

---
+++ Projects/TEAM-2/Mertsalova_Iryna/Tasks/Task301/Classes/Shop.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task301.Classes
+{
+    public class Shop
+    {
+        private List<Product> products;
+
+        public int getSize()
+        {
+            return products.Count;
+        }
+
+        public List<Product> GetList()
+        {
+            return products;
+        }
+
+        public Shop()
+        {
+            products = new List<Product>();
+        }
+
+        public void addProduct(Product item)
+        {
+            products.Add(item);
+        }
+
+        public void deleteProd(int id)
+        {
+            var delProduct = products.FirstOrDefault(x => id == x.getId());
+            products.Remove(delProduct);
+        }
+
+        public void printList()
+        {
+            foreach (var item in products)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine(" -----------------------------------------");
+        }
+
+
+    }
+}
