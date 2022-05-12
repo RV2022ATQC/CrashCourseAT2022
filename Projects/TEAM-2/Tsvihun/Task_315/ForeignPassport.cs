1ac4a627f1032830f5d0557d5b6e06ffd3a5dd99
LoveMyDaughter <tsav1986@gmail.com>
2022-05-12 12:51:42 +0300

    Tsvihun start task 315

---
+++ Projects/TEAM-2/Tsvihun/Task_315/ForeignPassport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_315
+{
+    class ForeignPassport : Passport
+    {
+        public string ForeignId { get; set; }
+
+
+        public ForeignPassport(int id, string foreignid, string type, string name, string lastname, string address, DateOnly birth) :base(id, type, name, lastname, address, birth )
+        {
+            ForeignId = foreignid;
+        
+
+        }
+
+        public new void ShowInfo()
+        {
+            Console.WriteLine($"Passport ID: {ForeignId} ({Type})\nName: {FirstName} {LastName}\nBirth: {Birth}\nAddress: {Address}");
+        }
+    }
+}
---
+++ Projects/TEAM-2/Tsvihun/Task_315/Program.cs
@@ -0,0 +1,29 @@
+﻿// 315
+// Створіть клас Passport (паспорт), який буде містити паспортну інформацію про громадянина України.
+// За допомогою механізму наслідування, реалізуйте клас ForeignPassport (закордонний паспорт) похідний від Passport.
+// Нагадаємо, що закордонний паспорт містить крім паспортних даних, також дані про візи, номер закордонного паспорта.
+using System;
+
+namespace Task_315
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Passport tsvihunav = new (
+                35149528, "Internal", "Anton", "Tsvihun", "Kostromska str. 3, Rivne, Ukraine", new DateOnly(1986,11,17));
+
+            tsvihunav.ShowInfo();
+            
+            Passport coleague1 = new (
+                95132311, "Internal", "Yulia", "Mykhalska", "Bukovynska str. 8/14, Rivne, Ukraine", new DateOnly(1988,11,04));
+
+            coleague1.ShowInfo();
+            
+            Passport coleague2Foreign = new ForeignPassport(1234, "E15142011", "International", "Yulia", "Mykhalska", "Bukovynska str. 8/14, Rivne, Ukraine", new DateOnly(1988,11,04));
+
+            coleague2Foreign.ShowInfo();
+
+        }
+    }
+}
