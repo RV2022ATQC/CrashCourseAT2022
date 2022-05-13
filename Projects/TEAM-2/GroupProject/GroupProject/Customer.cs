e8e1d85d180a5b264f206e71d44615ebc641e570
ShiroMx <tsyhanchuk_ak21@nuwm.edu.ua>
2022-05-13 17:39:02 +0300

    Some work in Customers

--- Projects/TEAM-2/GroupProject/GroupProject/Customer.cs
+++ Projects/TEAM-2/GroupProject/GroupProject/Customer.cs
@@ -7,7 +7,31 @@ using System.Threading.Tasks;
 
 namespace GroupProject
 {
-    public class Customer
+    public class Customers
     {
+       public string Name;
+       public float money;
+
+       public Customers(string Name, float money)
+        {
+            this.Name = Name;
+            this.money = money;
+        }
+       
+    public string Set_Name(string Name)
+        {
+            this.Name = Name;
+
+            return Name.ToString();
+        }
+
+    public float Set_money(float money)
+        {
+            this.money = money;
+
+            return money.ToString();
+        }
+
+     Customers new_customer = new Customers();
     }
 }
