563873d225f761fdec9d063c540d2ab81c26d169
Vlad <vladklopotenko@gmail.com>
2022-05-15 22:08:31 +0200

    Add Task 317

---
+++ Projects/TEAM-2/Klopotenko/317/317/Trapeze.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace _317
+{
+    internal class Trapeze : Figure
+    {
+        public Trapeze(double a, double b, double h) : base(a)
+        {
+            this.b = b;
+            this.h = h;
+        }
+
+        public override void Square()
+        {
+            base.Square();
+            double Square = (a + b) * h / 2;
+            Console.WriteLine($"trapeze is {Square}");
+        }
+    }
+}
