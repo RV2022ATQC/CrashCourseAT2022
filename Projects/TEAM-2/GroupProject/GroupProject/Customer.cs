031ff29941e5b59bb1ef3340159112fe60a15dcd
LoveMyDaughter <tsav1986@gmail.com>
2022-05-16 15:37:25 +0300

    Modified Program.sc

--- Projects/TEAM-2/GroupProject/GroupProject/Customer.cs
+++ Projects/TEAM-2/GroupProject/GroupProject/Customer.cs
@@ -9,5 +9,29 @@ namespace GroupProject
 {
     public class Customer
     {
+        public string Name;
+        public float money;
+
+        public Customer(string Name, float money)
+        {
+            this.Name = Name;
+            this.money = money;
+        }
+
+        public string Set_Name(string Name)
+        {
+            this.Name = Name;
+
+            return Name.ToString();
+        }
+
+        public float Set_money(float money)
+        {
+            this.money = money;
+
+            return money;
+        }
+
+
     }
 }
