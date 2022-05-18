f4a5c5ef62eb26df2aea51ef8318b87cefebcf2a
sofiaso <91011159+sofiaso@users.noreply.github.com>
2022-05-18 18:08:49 +0300

    Add class bus

---
+++ Projects/TEAM-2/Stetsyuk_S/Main Task/Main Task/Bus.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Serialization;
+
+namespace final_task
+{
+    [Serializable]
+    public class Bus : Vehicle
+    {
+        public Bus() { }
+
+        public Bus(string Brand, uint Year_of_production, string Model, uint Race) : base(Brand, Year_of_production, Model)
+        {
+            this.Race = Race;
+        }
+
+        [XmlElement()]
+        public uint Race { get; set; }
+
+        public override string ToString()
+        {
+            return ("Bus" + "\tBrand: " + Brand + "\tModel: " + Model + "\tYear of production: " + Year_of_production + "\tRace: " + Race);
+        }
+        public override bool Equals(object? obj)
+        {
+            return (obj as Bus)?.Brand == this.Brand &&
+                   (obj as Bus)?.Model == this.Model &&
+                   (obj as Bus)?.Year_of_production == this.Year_of_production &&
+                   (obj as Bus)?.Race == this.Race;
+        }
+    }
+}
