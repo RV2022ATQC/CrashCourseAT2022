b573636bb5f83436f6b3986bdfa4a1dff1bc0844
LoveMyDaughter <tsav1986@gmail.com>
2022-05-18 17:55:25 +0300

    Start project

---
+++ Projects/TEAM-2/Tsvihun/Task_401_Selenium/Task_401_Selenium/Program.cs
@@ -0,0 +1,16 @@
+﻿// Search product and add to cart positive test
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_401_Selenium
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
