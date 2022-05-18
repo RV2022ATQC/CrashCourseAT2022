cd10b1ac7d336f841eed20f104f2229496192e1a
andrii <m_a_0@ukr.net>
2022-05-18 21:24:24 +0300

    fix (Added value token in the url, Pointed sections  AAA in all tests)
    1. Added value token in the url
    string url = "http://127.0.0.1/opencart/index.php?route=api/cart/add&api_token=" + JsonToken; ;
    var client = new RestClient(url);
    2. Renamed last test
    I check if the cart is empty after deleting an item.
    3 Pointed sections  Arrange/Act/Assert in all tests

--- Projects/TEAM-3/Mykhailenko/QA2022_04_RestAPI/RestAPI/RestAPI.cs
+++ Projects/TEAM-3/Mykhailenko/QA2022_04_RestAPI/RestAPI/RestAPI.cs
@@ -22,6 +22,9 @@ namespace RestAPI
         [Test, Order(1)]
         public void VerifyLogin()
         {
+            //Arrange
+            // enter your token from adminpanel bitnami
+            string token_str = "hld6KM80jL5qlPzAxylp7rEzSGYKpf5hlDKhkGf17Sfyp8sUF0Rgj5MxynIbHi65vKx4sodqtGIurzDD0eg12FJJd0xz4dUOHpbGIvruRBe6SWCaD7w8OcJg6orBu2OUWKOgJjxvbugaPUA8VItSqsZubVuUYuw6h2FZi6Uh08tAWculbRoG32YB8WhFAI0NGJCfKGcFwsoUjfvr5N3uW7wgvkGmaDpvqaXE5lRcIlqb7HgKSw7B4a1kXsGsFm3b";
             log.Info("Start VerifyLogin test");
             // edit adress api/login
             var client = new RestClient("http://127.0.0.1/opencart/index.php?route=api/login");
@@ -32,10 +35,11 @@ namespace RestAPI
             request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
             request.AddHeader("Cookie", "OCSESSID=f778808d6c629abadee364daba; currency=USD; language=en-gb");
             request.AddParameter("username", "Default");
+            request.AddParameter("key", token_str);
 
-            // enter your token from adminpanel bitnami
-            request.AddParameter("key", "hld6KM80jL5qlPzAxylp7rEzSGYKpf5hlDKhkGf17Sfyp8sUF0Rgj5MxynIbHi65vKx4sodqtGIurzDD0eg12FJJd0xz4dUOHpbGIvruRBe6SWCaD7w8OcJg6orBu2OUWKOgJjxvbugaPUA8VItSqsZubVuUYuw6h2FZi6Uh08tAWculbRoG32YB8WhFAI0NGJCfKGcFwsoUjfvr5N3uW7wgvkGmaDpvqaXE5lRcIlqb7HgKSw7B4a1kXsGsFm3b");
+            //Act
             IRestResponse response = client.Execute(request);
+
             Console.WriteLine(response.Content);
             string source = response.Content;
             dynamic data = JsonConvert.DeserializeObject(source);
@@ -44,71 +48,105 @@ namespace RestAPI
             Console.WriteLine(data.api_token);
             log.Info("Api_token"+ data.api_token);
 
+            //Assert
+            Assert.IsNotEmpty(JsonToken);
         }
 
         [Test, Order(2)]
         public void AddIPhoneInCart()
         {
+            //Arrange
             log.Info("Start AddIPhoneInCart");
-            var client = new RestClient("http://127.0.0.1/opencart/index.php?route=api/cart/add&api_token=bc15f5c28a9f0fe7b095681c77");
+            string url = "http://127.0.0.1/opencart/index.php?route=api/cart/add&api_token=" + JsonToken; ;
+            var client = new RestClient(url);
             client.Timeout = -1;
             var request = new RestRequest(Method.POST);
             request.AddHeader("Cookie", "OCSESSID=f778808d6c629abadee364daba; currency=USD; language=en-gb");
             request.AlwaysMultipartFormData = true;
             request.AddParameter("product_id", "40");
             request.AddParameter("quantuty", "1");
+
+            //Act
             IRestResponse response = client.Execute(request);
             Console.WriteLine(response.Content);
             log.Info(response.Content);
+
+            //Assert
+            Assert.True(response.Content.Contains("Success"));
         }
 
         [Test, Order(3)]
         public void VerifyAddIPoneInCartFromAPI()
         {
-            
+            //Arrange
             string ExpectedValue = "iPhone";
+            string url = "http://127.0.0.1/opencart/index.php?route=api/cart/products/&api_token=" + JsonToken; ;
 
             log.Info("Start VerifyAddIPoneInCartFromAPI");
-            var client = new RestClient("http://127.0.0.1/opencart/index.php?route=api/cart/products/&api_token=bc15f5c28a9f0fe7b095681c77");
+            var client = new RestClient(url);
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
             request.AddHeader("Cookie", "OCSESSID=f778808d6c629abadee364daba; currency=USD; language=en-gb");
+
+            //Act
             IRestResponse response = client.Execute(request);
             Console.WriteLine(response.Content);
             string source = response.Content;
             dynamic data = JsonConvert.DeserializeObject(source);
             cart_id = data.products[0].cart_id;
             string ActualValue = data.products[0].name;
-            Assert.AreEqual(ActualValue, ExpectedValue);
             log.Info(response.Content);
+
+            //Assert
+            Assert.AreEqual(ActualValue, ExpectedValue);
         }
 
         [Test, Order(4)]
         public void  DeleteItemsFromCart()
         {
+            //Arrange
+            string url = "http://127.0.0.1/opencart/index.php?route=api/cart/remove&=4&api_token=" + JsonToken; ;
+
             log.Info("Start DeleteItemsFromCart");
-            var client = new RestClient("http://127.0.0.1/opencart/index.php?route=api/cart/remove&=4&api_token=bc15f5c28a9f0fe7b095681c77");
+            var client = new RestClient(url);
             client.Timeout = -1;
             var request = new RestRequest(Method.POST);
             request.AddHeader("Cookie", "OCSESSID=f778808d6c629abadee364daba; currency=USD; language=en-gb");
             request.AlwaysMultipartFormData = true;
             request.AddParameter("key", cart_id);
+
+            //Act
             IRestResponse response = client.Execute(request);
             Console.WriteLine(response.Content);
             log.Info(response.Content);
+
+            //Assert
+            Assert.True(response.Content.Contains("Success"));
         }
 
         [Test, Order(5)]
-        public void ViewItemsInCartAfterDelete()
+        public void VerifyEmptyCartAfterDeleteItem()
         {
+            //Arrange
+            string url = "http://127.0.0.1/opencart/index.php?route=api/cart/products/&api_token=" + JsonToken; ;
+
             log.Info("Start ViewItemsInCartAfterDelete");
-            var client = new RestClient("http://127.0.0.1/opencart/index.php?route=api/cart/products/&api_token=bc15f5c28a9f0fe7b095681c77");
+            var client = new RestClient(url);
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
             request.AddHeader("Cookie", "OCSESSID=f778808d6c629abadee364daba; currency=USD; language=en-gb");
+
+            //Act
             IRestResponse response = client.Execute(request);
+
+            string source = response.Content;
+            dynamic data = JsonConvert.DeserializeObject(source);
             Console.WriteLine(response.Content);
             log.Info(response.Content);
+
+            //Assert
+            Assert.IsEmpty(data.products);
+
         }
     }
 }
