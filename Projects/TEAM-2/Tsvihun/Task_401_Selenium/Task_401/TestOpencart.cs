1e2057b7596cc123fc83b2bd236a009daf11b9ba
LoveMyDaughter <tsav1986@gmail.com>
2022-05-19 16:17:05 +0300

    Update TestOpencart.cs

--- Projects/TEAM-2/Tsvihun/Task_401_Selenium/Task_401/TestOpencart.cs
+++ Projects/TEAM-2/Tsvihun/Task_401_Selenium/Task_401/TestOpencart.cs
@@ -12,7 +12,8 @@ namespace Task_401_Selenium
 
     class TestOpencart
     {
-        private static readonly string _urlLocal = "http://localhost/shop/index.php?route=account/login";
+        private static readonly string _url = "http://localhost/shop/index.php";
+        private static readonly string _urlLogin = "http://localhost/shop/index.php?route=account/login";
         //private static readonly string _urlExternal = "https://demo.opencart.com/index.php?route=account/login";
         
         IWebDriver driver;
@@ -31,23 +32,37 @@ namespace Task_401_Selenium
             // 3.Press "Login" button
             // 4.Check redirection 
 
+
+            // Arrange
             string userEmail = "antront@ukr.net";
             string userPassword = "ront";
             string accountUrl = "http://localhost/shop/index.php?route=account/account";
 
-            driver.Navigate().GoToUrl(_urlLocal);
-
-            // Fill in email & password
+            // Act
+            driver.Navigate().GoToUrl(_urlLogin);
             driver.FindElement(By.Id("input-email")).SendKeys(userEmail);
             driver.FindElement(By.Id("input-password")).SendKeys(userPassword);
-
-            // Press "Login"
             driver.FindElement(By.CssSelector("#content > div > div:nth-child(2) > div > form > input")).Click();
 
+            // Assert
             string currentUrl = driver.Url;
-
-            // Check if redirected
             Assert.AreEqual(accountUrl, currentUrl);
+        }
+
+        [Test]
+        public void TestSearchProductInOpencart()
+        {
+            // Arrange
+
+            // Act
+            driver.Navigate().GoToUrl(_url);
+            driver.FindElement(By.XPath("/html/body/header/div/div/div[2]/div")).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.XPath("/html/body/header/div/div/div[2]/div")).SendKeys("iphone");
+            Thread.Sleep(3000);
+
+            // Assert
+
         }
 
         [OneTimeTearDown]
