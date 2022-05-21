7a6854571f573f405ad086b96616217b7663d74f
LoveMyDaughter <tsav1986@gmail.com>
2022-05-21 23:56:49 +0300

    Tsvihun start task 322

---
+++ Projects/TEAM-2/Tsvihun/Task_322/Goods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_322
+{
+    internal class Goods
+    {
+        public string Name { get; set; }
+        public decimal EntryPrice { get; set; }
+        public DateTime EntryDate { get; set; }
+        public int Count { get; set; }
+
+        public Goods(string name, decimal price, DateTime date, int amount)
+        {
+            Name = name;
+            EntryPrice = price;
+            EntryDate = date;
+            Count = amount;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}\nQuantity: {Count}\nPrice: {EntryPrice}\nEntryDate: {EntryDate}";
+        }
+    }
+}
---
+++ Projects/TEAM-2/Tsvihun/Task_322/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_322
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Goods goods = new Goods("Cup", 40, new DateTime(2021,09,10), 20);
+            Console.WriteLine(goods.ToString());
+        }
+    }
+}
