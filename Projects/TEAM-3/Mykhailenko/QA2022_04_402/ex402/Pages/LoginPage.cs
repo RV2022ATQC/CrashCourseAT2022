07944367def854973383273fb668fe375bf154b3
andrii <m_a_0@ukr.net>
2022-05-22 15:12:31 +0300

    add ex402
    add ex402

---
+++ Projects/TEAM-3/Mykhailenko/QA2022_04_402/ex402/Pages/LoginPage.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+
+namespace ex402.Pages
+{
+    internal class LoginPage : MainPage
+    {
+
+        public void LoginUser(string _url, string _email, string _pass)
+        {
+            driver.Url = _url;
+            IWebElement my_acc = driver.FindElement(By.ClassName("dropdown"));
+            my_acc.Click();
+            IWebElement login = driver.FindElement(By.LinkText("Login"));
+            login.Click();
+            IWebElement user = driver.FindElement(By.Name("email"));
+            IWebElement pass = driver.FindElement(By.Name("password"));
+            IWebElement btn = driver.FindElement(By.XPath("//*[@id=\"content\"]/div/div[2]/div/form/input"));
+            user.Clear();
+            user.SendKeys(_email);
+            pass.Clear();
+            pass.SendKeys(_pass);
+            btn.Click();
+        }
+        public void CheckThatRoute(string _exp_route)
+        {
+            Assert.That(driver.Url, Does.Contain(_exp_route));
+        }
+    }
+}
