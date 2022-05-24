c1d97fb85598e8cf37a81e51d8ea270bf91797b7
LoveMyDaughter <tsav1986@gmail.com>
2022-05-24 16:04:08 +0300

    Start test project

---
+++ Projects/TEAM-2/Tsvihun/Task_322_Tests/TestsFoodGoods.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+
+namespace Task_322_Tests
+{
+    [TestFixture]
+    public class TestsFoodGoods
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Console.WriteLine("Start test.");
+        }
+    }
+}
