1d446f394e84ee3a0cb92ff107fcd42ad705bc77
LoveMyDaughter <tsav1986@gmail.com>
2022-05-30 01:41:50 +0300

    Tsvihun RestAPI Add DB tests

--- Projects/TEAM-2/Tsvihun/RestAPI/RestAPI/RestAPI_Tests/Database.cs
+++ Projects/TEAM-2/Tsvihun/RestAPI/RestAPI/RestAPI_Tests/Database.cs
@@ -11,37 +11,49 @@ namespace RestAPI_Tests
     internal class Database
     {
         private static readonly string _connectionString = "server=localhost; userid=root; database=shop";
-     
-        public static void ExecuteQuery()
+        
+        static void ShowHeaders(MySqlDataReader reader)
         {
-            var queryString = $@"SELECT * FROM `oc_cart`";
+            var columnNames = Enumerable.Range(0, reader.FieldCount)
+                        .Select(reader.GetName)
+                        .ToArray();
+            foreach (var column in columnNames) { Console.Write($"{column}\t"); }
+            Console.WriteLine();
+        }
 
+        static void ShowRows(MySqlDataReader reader)
+        {
+            while (reader.Read())
+            {
+                var row = Enumerable.Range(0, reader.FieldCount)
+                    .Select(reader.GetValue).ToArray();
+                foreach (var column in row) { Console.Write($"{column}\t"); }
+                Console.WriteLine();
+            }
+        }
+
+        public static void ExecuteQuery(string queryString)
+        {
             using (var connection = new MySqlConnection(_connectionString))
             {
                 var command = new MySqlCommand(queryString, connection);
                 command.Connection.Open();
                 MySqlDataReader reader = command.ExecuteReader();
 
+                // If the answer of execute query in not empty - show column names & all rows below 
                 if (reader.HasRows)
                 {
-                    while (reader.Read())
-                    {
-                        var value = reader.GetString(3);    // This column(session_id) is token
-                        Console.WriteLine(value);
-                    }
+                    //ShowHeaders(reader);
+                    ShowRows(reader);
                 }
-                else
-                {
-                    Console.WriteLine("No rows found.");
-                }
+                else { Console.WriteLine("No rows found."); }
             }
         }
 
-        //public static void GetFirstnameById(string userId)
-        //{
-        //    var queryString = $@"SELECT * FROM `oc_cart`";
-
-        //    ExecuteQuery(queryString, "SurveyTemplate was added");
-        //}
+        public static void Show_oc_cart()
+        {            
+            string show_oc_cart = $@"SELECT * FROM `oc_cart`";
+            ExecuteQuery(show_oc_cart);
+        }
     }
 }
--- Projects/TEAM-2/Tsvihun/RestAPI/RestAPI/RestAPI_Tests/OpencartTests.cs
+++ Projects/TEAM-2/Tsvihun/RestAPI/RestAPI/RestAPI_Tests/OpencartTests.cs
@@ -24,113 +24,137 @@ namespace RestAPI_Tests
         [Test, Order(1)]
         public void Opencart_CheckLogin()
         {
+            // Arrange
             Console.WriteLine("Check API login to Opencart. Get token.");
-
             var client = new RestClient($"{baseUrl}index.php?route=api/login");
             client.Timeout = -1;
             var request = new RestRequest(Method.POST);
-            //request.AddHeader("username", "Default");
-            //request.AddHeader("key", "j0M724bQvkcrVst6XJfafMfzNShlBx0W5PGnh2WkBy4sxPrpCk3hSqA2xAm2AHPZXynNAQEPJcOHgdPTuLSZdM1Il4wZ6vgk6Fvye0i5jSNEFIFk5s7tmKIMSItCeyYM2kLEMqaLe5mSgKOyIRTaGZq4TdPd44DleW2Q3qqfEW3jean6FbsPaCSbQeQWRqMXL5bmNWb4oFWufPMkozLdVofXUU3pY2lEZc32p6Tta1HzPUoD0APR4fCRGksv8RK6");
-            //request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+
+            // Act
             request.AddParameter("username", "Default");
             request.AddParameter("key", apiKey);
             IRestResponse response = client.Execute(request);
-            Console.WriteLine(response.Content);
-
             string source = response.Content;
             dynamic data = JsonConvert.DeserializeObject(source);
-
             JsonToken = data.api_token;
+
+            // Assert
+            Assert.That(!String.IsNullOrEmpty(JsonToken));
+            Console.WriteLine(response.Content);
         }
 
         [Test, Order(2)]
         public void Opencart_AddProductToCart()
         {
+            // Arrange
+            Console.WriteLine("Check adding product to cart.");
             var client = new RestClient($"{baseUrl}index.php?route=api/cart/add&api_token={JsonToken}");
             client.Timeout = -1;
             var request = new RestRequest(Method.POST);
+
+            // Act
             request.AddHeader("Cookie", $"OCSESSID={JsonToken}; currency=USD; language=en-gb");
             request.AlwaysMultipartFormData = true;
             request.AddParameter("product_id", "48");
             request.AddParameter("quantity", "20");
             IRestResponse response = client.Execute(request);
-            Console.WriteLine(response.Content);
-
+            
+            // Assert
             Assert.That(response.Content.Contains("Success"));
+            Console.WriteLine(response.Content);
         }
 
+        // Always green test
         [Test, Order(3)]
-        public void Opencart_GetCartContent()
+        public void Opencart_CheckAddProductToCartInDB()
         {
-            var client = new RestClient($"{baseUrl}index.php?route=api/cart/products/&api_token={JsonToken}");
-            client.Timeout = -1;
-            var request = new RestRequest(Method.GET);
-            request.AddHeader("Cookie", $"OCSESSID={JsonToken}; currency=USD; language=en-gb");
-            IRestResponse response = client.Execute(request);
-
-            string source = response.Content;
-            dynamic data = JsonConvert.DeserializeObject(source);
-
-            cartId = data.products[0].cart_id;
-            Console.WriteLine($"Cart #{cartId} content:\n{source}");
-            Assert.That(!source.Contains("Warning: You do not have permission to access the API!"));
+            Database.Show_oc_cart();
         }
 
         [Test, Order(4)]
+        public void Opencart_GetCartContent()
+        {
+            // Arrange
+            Console.WriteLine("Getting cart content.");
+            var client = new RestClient($"{baseUrl}index.php?route=api/cart/products/&api_token={JsonToken}");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+
+            // Act
+            request.AddHeader("Cookie", $"OCSESSID={JsonToken}; currency=USD; language=en-gb");
+            IRestResponse response = client.Execute(request);
+            string source = response.Content;
+            dynamic data = JsonConvert.DeserializeObject(source);
+            cartId = data.products[0].cart_id;
+
+            // Assert
+            Assert.That(!source.Contains("Warning: You do not have permission to access the API!"));
+            Console.WriteLine($"Cart #{cartId} content:\n{source}");
+        }
+
+        [Test, Order(5)]
         public void Opencart_ChangeCart()
         {
+            // Arrange
+            Console.WriteLine("Changing cart.");
             var client = new RestClient($"{baseUrl}index.php?route=api/cart/edit&api_token={JsonToken}");
             client.Timeout = -1;
             var request = new RestRequest(Method.POST);
+
+            // Act
             request.AddHeader("Cookie", $"OCSESSID={JsonToken}; currency=USD; language=en-gb");
             request.AlwaysMultipartFormData = true;
             request.AddParameter("key", cartId);
             request.AddParameter("quantity", "10");
             IRestResponse response = client.Execute(request);
             
-            Console.WriteLine($"{response.Content}\nCartId = {cartId}");
-
+            // Assert
             Assert.That(response.Content.Contains("Success"));
+            Console.WriteLine($"{response.Content}\nCartId = {cartId}");
         }
 
-        [Test,Order(5)]
+        [Test,Order(6)]
         public void Opencart_RemoveFromCart()
         {
+            // Arrange
+            Console.WriteLine("Check remove from cart.");
             var client = new RestClient($"{baseUrl}index.php?route=api/cart/remove&api_token={JsonToken}");
             client.Timeout = -1;
             var request = new RestRequest(Method.POST);
+
+            // Act
             request.AddHeader("Cookie", $"OCSESSID={JsonToken}; currency=USD; language=en-gb");
             request.AlwaysMultipartFormData = true;
             request.AddParameter("key", cartId);
             IRestResponse response = client.Execute(request);
 
-            Console.WriteLine($"{response.Content}\nCartId = {cartId}");
-
+            // Assert
             Assert.That(response.Content.Contains("Success"));
+            Console.WriteLine($"{response.Content}\nCartId = {cartId}");
         }
 
-        [Test, Order(6)]
+        [Test, Order(7)]
         public void Opencart_CheckIfEmptyCart()
         {
+            // Arrange
+            Console.WriteLine("Check if cart is empty");
             var client = new RestClient($"{baseUrl}index.php?route=api/cart/products/&api_token={JsonToken}");
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
+
+            // Act
             request.AddHeader("Cookie", $"OCSESSID={JsonToken}; currency=USD; language=en-gb");
             IRestResponse response = client.Execute(request);
 
+            // Assert
+            Assert.That(response.Content.Contains("\"title\":\"Total\",\"text\":\"$0.00\""));
             Console.WriteLine($"Cart #{cartId} content:\n{response.Content}");
-
-            //Assert.That(!source.Contains("Warning: You do not have permission to access the API!"));
         }
 
-
-
-
-
-        [Test]
-        public void TestDB()
+        [Test, Order(8)]
+        public void Opencart_CheckIfEmptyCartInDB()
         {
-            Database.ExecuteQuery();
+            Database.Show_oc_cart();
         }
     }
 }
