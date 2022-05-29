ecaa61b451eca7664d435ec9b23f9a0ac2659143
Yurii Turbal <103252117+TurbalY@users.noreply.github.com>
2022-05-30 01:12:28 +0300

    first

---
+++ Projects/TEAM-1/team1_group_proj/TestFramework/TestFramework/Pages/ATopComponent.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using crashCourse2022.Tools;
+using crashCourse2022.Tools.Find;
+
+namespace crashCourse2022.Pages
+{
+    
+        public abstract class ATopComponent
+    {
+        public const string SRC_ATTRIBUTE = "src";
+        public const string VALUE_ATTRIBUTE = "value";
+        //
+        protected ISearch Search { get; private set; }
+        //protected IWebDriver driver;
+        //
+        public IWebElement TitleLabel //{ get; private set; }
+            //{ get { return driver.FindElement(By.XPath("//div[@id='header']/div/h3/..")); } }
+            { get { return Search.XPath("//div[@id='header']/div/h3/.."); } }
+        public SelectElement ChangeLanguage //{ get; private set; }
+            //{ get { return new SelectElement(driver.FindElement(By.Id("changeLanguage"))); } }
+            { get { return new SelectElement(Search.Id("changeLanguage")); } }
+
+        //protected ATopComponent(IWebDriver driver)
+        protected ATopComponent()
+        {
+            Search = Application.Get().Search;
+            //this.driver = driver;
+            //
+            // Classic Page Object
+            //TitleLabel = driver.FindElement(By.XPath("//div[@id='header']/div/h3/.."));
+            //ChangeLanguage = new SelectElement(driver.FindElement(By.Id("changeLanguage")));
+            //
+            VerifyWebElements();
+        }
+
+        private void VerifyWebElements()
+        {
+            IWebElement temp = TitleLabel;
+            SelectElement select = ChangeLanguage;
+        }
+
+        // PageObject Atomic
+
+        // TitleLabel
+        public string GetTitleLabelText()
+        {
+            return TitleLabel.Text;
+        }
+
+        public void ClickTitleLabel()
+        {
+            TitleLabel.Click();
+        }
+
+        // ChangeLanguage
+        
+
+        // Business Logic
+    }
+}
